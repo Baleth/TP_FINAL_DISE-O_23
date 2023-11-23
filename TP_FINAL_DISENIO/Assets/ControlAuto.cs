e6dfa4e69d17b25436baec6980fee530a74fbb7f
Baleth <balethleo@gmail.com>
2023-11-23 18:14:35 -0300

    SUBO BASE DE PROTOTIPO
    PAL AÑO

---
+++ TP_FINAL_DISENIO/Assets/ControlAuto.cs
@@ -0,0 +1,171 @@
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlAuto : MonoBehaviour
+{
+    private bool band;
+    private bool fren;
+    private Touch touch;
+    [System.Serializable]
+    public class infoEje
+    {
+        public WheelCollider ruedaIzquierda;
+        public WheelCollider ruedaDerecha;
+        public bool motor;
+        public bool direccion;
+
+    }
+    public List<infoEje> infoEjes;
+    public float maxMotorTorsion;
+    public float maxAnguloDeGiro;
+
+    //Los collider
+    [SerializeField] private WheelCollider frontLeftWheelCollider;
+    [SerializeField] private WheelCollider frontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider;
+    [SerializeField] private WheelCollider rearRightWheelCollider;
+
+    Rigidbody body;
+    private void Start()
+    {
+
+        Button boton;
+
+        body = GetComponent<Rigidbody>();
+    }
+    void posRuedas(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+        Transform ruedaVisual = collider.transform.GetChild(0);
+        Vector3 posicion;
+        Quaternion rotacion;
+        collider.GetWorldPose(out posicion, out rotacion);
+        ruedaVisual.transform.position = posicion;
+        ruedaVisual.transform.rotation = rotacion;
+
+    }
+    void toqueFrenar()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began && touch.position.y < Screen.height / 2)
+            {
+                frontLeftWheelCollider.brakeTorque = 150000;
+                frontRightWheelCollider.brakeTorque = 150000;
+                rearLeftWheelCollider.brakeTorque = 150000;
+                rearRightWheelCollider.brakeTorque = 150000;
+            }
+
+            if (touch.phase == TouchPhase.Ended && touch.position.y < Screen.height / 2)
+            {
+                frontLeftWheelCollider.brakeTorque = 0;
+                frontRightWheelCollider.brakeTorque = 0;
+                rearLeftWheelCollider.brakeTorque = 0;
+                rearRightWheelCollider.brakeTorque = 0;
+            }
+        }
+
+
+    }
+    void toqueAcelerar()
+        {
+            if (Input.touchCount > 0)
+            {
+                touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began && touch.position.y > Screen.height / 2)
+            {
+                frontLeftWheelCollider.motorTorque = maxMotorTorsion;
+                frontRightWheelCollider.motorTorque = maxMotorTorsion;
+                rearLeftWheelCollider.motorTorque = maxMotorTorsion;
+                rearRightWheelCollider.motorTorque = maxMotorTorsion;
+            }
+
+            if (touch.phase == TouchPhase.Ended && touch.position.y > Screen.height / 2)
+            {
+                frontLeftWheelCollider.motorTorque = 0;
+                frontRightWheelCollider.motorTorque = 0;
+                rearLeftWheelCollider.motorTorque = 0;
+                rearRightWheelCollider.motorTorque = 0;
+            }
+          }
+        }   
+    
+   
+    private void FixedUpdate()
+    {
+        toqueAcelerar();
+        toqueFrenar();
+          Debug.Log((Mathf.Sqrt(Mathf.Pow(body.velocity.x,2)+ Mathf.Pow(body.velocity.z, 2)))*6);
+       // ControlarFrenado();
+
+        //float motor = maxMotorTorsion * Input.GetAxis("Vertical");
+        // float direccion = maxAnguloDeGiro * Input.GetAxis("Horizontal");
+
+
+        //float motor = maxMotorTorsion * Input.acceleration.y*-1;
+        //float motor = 0;
+        //if (band == true)
+        //{
+        //    motor = maxMotorTorsion * 1;
+        //}
+        //else {
+        //    motor = 0;
+        //}
+        //if (fren == true)
+        //{
+        //    motor = -2000;
+        //}
+
+        float direccion = maxAnguloDeGiro * Input.acceleration.x;
+        foreach (infoEje ejesInfo in infoEjes)
+        {
+            //paraGirar las ruedas
+            if (ejesInfo.direccion)
+            {
+                ejesInfo.ruedaIzquierda.steerAngle = direccion;
+                ejesInfo.ruedaDerecha.steerAngle = direccion;
+            }
+            //Mover las ruedas
+            //if (ejesInfo.motor)
+            //{
+            //    ejesInfo.ruedaIzquierda.motorTorque = motor;
+            //    ejesInfo.ruedaDerecha.motorTorque = motor;
+            //}
+            posRuedas(ejesInfo.ruedaIzquierda);
+            posRuedas(ejesInfo.ruedaDerecha);
+
+        }
+
+    }
+
+    public void Positivo()
+    {
+        band = true;
+
+    }
+    public void Negativo()
+    {
+        band = false;
+    }
+    public void FrenoPos()
+    {
+        fren = true;
+
+    }
+
+    public void Presiono()
+    {
+        Debug.Log("Estoy presionando");
+    }
+    public void deje()
+    {
+        Debug.Log("Deje de presionar");
+    }
+
+}
