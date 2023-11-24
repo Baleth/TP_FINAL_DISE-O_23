407671b5b404b22c0c3ab83b813c4a1105ccbfa8
Baleth <balethleo@gmail.com>
2023-11-24 15:28:42 -0300

    asdasd
    asdasd

--- TP_FINAL_DISENIO/Assets/VelocidadMaximaScript.cs
+++ TP_FINAL_DISENIO/Assets/VelocidadMaximaScript.cs
@@ -6,20 +6,27 @@ public class VelocidadMaximaScript : MonoBehaviour
 {
      float velocidadACambiar;
   GameManager gameManager;
+    alertaScript alerta;
   ControlAuto controlAuto;
     void Start()
     {
+        alerta = FindAnyObjectByType<alertaScript>().GetComponent<alertaScript>();
+
         controlAuto = FindAnyObjectByType<ControlAuto>().GetComponent<ControlAuto>();
         gameManager = FindAnyObjectByType<GameManager>().GetComponent<GameManager>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
-            if (controlAuto.getVelocity() > 60)
-            {
-                gameManager.QuitarPuntaje(10,"velocidad");
-            }
+
+        if (controlAuto.getVelocity() > 60)
+        {
+            gameManager.QuitarPuntaje(10, "velocidad");
+        }
+        else {
+            StartCoroutine(alerta.CambiarTexto("velocidadBien"));
+            
+        }
         
     }
 }
